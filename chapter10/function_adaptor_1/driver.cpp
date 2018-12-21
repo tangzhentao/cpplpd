ab4bcdde06ab816b78329c984bac3a1b189b0ba9
tang <itangzhentao@126.com>
2018-12-21 11:47:08 +0800

    m

--- chapter10/function_adaptor_1/driver.cpp
+++ chapter10/function_adaptor_1/driver.cpp
@@ -19,13 +19,41 @@ int main()
 	vector<int> s(a, a+N);
 
 	// 打印向量s的元素
+	cout << "向量s中的元素：" << endl;
 	copy(s.begin(), s.end(), ostream_iterator<int>(cout, " "));
+	cout << endl;
 
-	vector<int>::iterator first = s.begin(), last = s.end();
-	vector<int>::iterator iter = find_if(first, last, bind2nd(g, 40));
 
-	cout << "检查迭代器first有没有被find_if改变" << endl;
-	cout << "first element: " << *first << endl;
+	int iToFind;
+	bool found = false;
+	do 
+	{
+		cout << "输入一个基数: ";
+		cin >> iToFind;
+		vector<int>::iterator first = s.begin(), last = s.end();
+		vector<int>::iterator iter = find_if(first, last, bind2nd(ptr_fun(g), iToFind));
+		if (iter == last)
+		{
+			cout << "没有找到比40大的元素" << endl;
+		} else
+		{
+			found = true;
+			cout << "第一比 " << iToFind << " 大的元素是 " << *iter << endl;
+		}
+
+		cout << "检查迭代器first有没有被find_if改变" << endl;
+		cout << "first element: " << *first << endl;
+	} while(false == found);
+
+	vector<int>::iterator iter = find_if(s.begin(), s.end(), not1( bind2nd(greater<int>(), 15) ) );
+	if (iter == s.end()) 
+	{
+		cout << "没有找到不大于15的元素" << endl;
+	} else 
+	{
+		cout << "第一个不大于15的元素是" << *iter << endl;
+	}
+
 
 	return 0;
 }
